0d918519f9dcb16477dca9a488a518529c66ff19
thamara.hessel <thamara.hessel@olx.com>
2020-05-28 22:54:15 +0100

    update print log exhibition

--- main.py
+++ main.py
@@ -4,44 +4,42 @@ import random
 import numpy
 import string
 
+LOG = 1
+
 def main():
-    p = int(sys.argv[1])
     m = sys.argv[2]
     message = numpy.fromstring(m, dtype=numpy.uint8)
+    logTrace("ASCII msg", message, LOG)
 
+    p = int(sys.argv[1])
     if not isPrime(p):
         print(p, "p is not a prime")
         return False
-    q = int((p - 1)/2)
 
+    logTrace("p", p, LOG)
+
+    q = int((p - 1)/2)
     if not isPrime(q):
         print(q, "q is not a prime")
         return False
+    logTrace("q", q, LOG)
 
-    print("p:", p)
-    print("q:", q)
     G = subgroup(q, p)
-    Zq = group(q)
-    print("Zq:", Zq)
-    print("subgroup:", G)
+    logTrace("subgroup", G, LOG)
 
-    print('ASCII msg:', message)
+    Zq = group(q)
+    logTrace("Zq", Zq, LOG)
 
     keys = keysGenerator(G,Zq,p)
-    print('Public and Private keys:',keys)
+    logTrace("Public and Private keys", keys, LOG)
 
     enc = encrypt(message, keys, G, Zq, p)
-    print('Encrypted message:', enc)
+    print("Encrypted message", enc)
 
     dec = decrypt(enc,p,G,keys)
-    print('Decrypted message:', dec)
+    print("Decrypted message", dec)
 
 def decrypt(enc,mod,G,keys):
-
-    #enc[u1,u2,e,v]
-    # publicKey[g1,g2,c,d1,d2,d3,h]
-    # privateKey[x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z]
-
     message = []
     for j in range(0, len(enc)):
 
@@ -59,7 +57,6 @@ def decrypt(enc,mod,G,keys):
             for i in range(0, mod):
                 if powMod(u1,z,mod)*i%mod == 1:
                     inv = i
-            #print(inv, inv*powMod(u1,z,mod)%mod)
             message.append((chr(G.index(e*inv%mod))))
         else:
             return 'rejected'
@@ -70,61 +67,61 @@ def decrypt(enc,mod,G,keys):
 def encrypt(message, keys, G, Zq, p):
     encrypt = []
     for i in range(0, len(message)):
-        print('message[i]',message[i])
+        logTrace("message[i]", message[i], LOG)
         m = G[(message[i])]
-        print('m',m)
+        logTrace("m", m, LOG)
         cipher = cipherText(Zq, p, keys, m)
-        print('CipherText:',cipher)
+        logTrace("CipherText", cipher, LOG)
         encrypt.append(cipher)
 
     return encrypt
 
 def cipherText(Zq, p, keys, m):
     r = random.choice(Zq)
-    print('Random r',r)
+    logTrace("Random r", r, LOG)
 
     g1 = keys[0][0]
-    print('g1',g1)
+    logTrace("g1", g1, LOG)
 
     u1 = powMod(g1,r,p)
-    print('u1',u1)
+    logTrace('u1',u1, LOG)
 
     g2 = keys[0][1]
-    print('g2',g2)
+    logTrace('g2',g2, LOG)
 
     u2 = powMod(g2,r,p)
-    print('u2',u2)
+    logTrace('u2',u2, LOG)
 
     h = keys[0][4]
-    print('h',h)
+    logTrace('h',h, LOG)
 
     e = (powMod(h,r,p)*m)%p
-    print('e', e)
+    logTrace('e', e, LOG)
 
     c = keys[0][2]
-    print('c',c)
+    logTrace('c',c, LOG)
 
     d1 = keys[0][3][0]
     d2 = keys[0][3][1]
     d3 = keys[0][3][2]
-    print('d1',d1)
-    print('d2',d2)
-    print('d3',d3)
+    logTrace('d1',d1, LOG)
+    logTrace('d2',d2, LOG)
+    logTrace('d3',d3, LOG)
 
     y1a = keys[1][2]
-    print('y1',y1a)
+    logTrace('y1',y1a, LOG)
     y2a = keys[1][3]
-    print('y2',y2a)
+    logTrace('y2',y2a, LOG)
 
     y1b = keys[1][4]
-    print('y1b',y1b)
+    logTrace('y1b',y1b, LOG)
     y2b = keys[1][5]
-    print('y2b',y2b)
+    logTrace('y2b',y2b, LOG)
 
     y1c = keys[1][6]
-    print('y1c',y1c)
+    logTrace('y1c',y1c, LOG)
     y2c = keys[1][7]
-    print('y2c',y2c)
+    logTrace('y2c',y2c, LOG)
 
     v = vGenerator(u1,u2,e,p, r, c, d1, d2, d3)
 
@@ -134,7 +131,8 @@ def keysGenerator(G,Zq,mod):
     g1 = random.choice(G)
     g2 = random.choice(G)
 
-    print('G random choice:',g1,g2)
+    logTrace('G random choice g1',g1, LOG)
+    logTrace('G random choice g2',g2, LOG)
 
     x1 = random.choice(Zq)
     x2 = random.choice(Zq)
@@ -147,19 +145,20 @@ def keysGenerator(G,Zq,mod):
     y2c = random.choice(Zq)
     z = random.choice(Zq)
 
-    print('Zq random choice:',x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z)
+    logTrace('Zq random choice x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z ',[x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z], LOG)
 
     c = ((powMod(g1,x1,mod) * powMod(g2,x2,mod))%mod)
-    print('c:',c)
+    logTrace('c',c, LOG)
+
 
     d1 = ((powMod(g1,y1a,mod) * powMod(g2,y2a,mod))%mod)
     d2 = ((powMod(g1,y1b,mod) * powMod(g2,y2b,mod))%mod)
     d3 = ((powMod(g1,y1c,mod) * powMod(g2,y2c,mod))%mod)
     d = [d1,d2,d3]
 
-    print('d:',d)
+    logTrace('d',d, LOG)
     h = powMod(g1,z,mod)
-    print('h:',h)
+    logTrace('h',h, LOG)
 
     publicKey = [g1,g2,c,d,h]
     privateKey = [x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z]
@@ -169,11 +168,11 @@ def keysGenerator(G,Zq,mod):
 def vGenerator(u1, u2, e, mod, r, c, d1, d2, d3):
     v = powMod(c, r, mod)
     v = ((v * powMod(d1,(u1*r), mod))%mod)
-    print('v-u1:',v)
+    logTrace('v-u1',v, LOG)
     v = ((v * powMod(d2,(u2*r), mod))%mod)
-    print('v-u2:',v)
+    logTrace('v-u2',v, LOG)
     v = ((v * powMod(d3,(e*r), mod))%mod)
-    print('v-e:',v)
+    logTrace('v-e',v, LOG)
 
     return v
 
@@ -203,7 +202,7 @@ def generator(pow, mod):
     for i in range(2, mod):
         g = powMod(i,pow,mod)
         if g == 1:
-            print("generator:", i)
+            logTrace('generator',i, LOG)
             return i
 
 
@@ -218,6 +217,11 @@ def isPrime(n) :
             return False
 
     return True
+
+def logTrace(traceKey, traceValue, enable) :
+    if (int(enable) == 1):
+        print(traceKey+" =>=>=> ", traceValue)
+
 
 #We choose a large prime p
 
