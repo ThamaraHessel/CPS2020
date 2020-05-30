a31a00bf437a922ef60cdd23912260e2c5bebd61
thamara.hessel <thamara.hessel@olx.com>
2020-05-30 21:17:16 +0100

    Update trace method and encoding

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ import sys
 import random
 import numpy
 
-LOG = 0
+ENABLED_TRACE = 0
+DECODE = "cp1252"
 
 def main():
     m = sys.argv[2]
-    message = numpy.fromstring(m, dtype=numpy.uint8)
-    logTrace("ASCII msg", message, LOG)
+    message = numpy.frombuffer(m.encode(), dtype=numpy.uint8)
+    showTrace("ASCII msg", message, ENABLED_TRACE)
 
     p = int(sys.argv[1])
     if not isPrime(p):
         print(p, "p is not a prime")
         return False
 
-    logTrace("p", p, LOG)
+    showTrace("p", p, ENABLED_TRACE)
 
     q = int((p - 1)/2)
     if not isPrime(q):
         print(q, "q is not a prime")
         return False
-    logTrace("q", q, LOG)
+    showTrace("q", q, ENABLED_TRACE)
 
     G = subgroup(q, p)
 
@@ -29,20 +30,23 @@ def main():
         print(len(G), "The subgroup must contain at least 255 to support ASCII")
         return False
 
-    logTrace("subgroup", G, LOG)
+    showTrace("subgroup", G, ENABLED_TRACE)
 
     Zq = group(q)
-    logTrace("Zq", Zq, LOG)
+    showTrace("Zq", Zq, ENABLED_TRACE)
 
     keys = keysGenerator(G,Zq,p)
-    logTrace("Public and Private keys", keys, LOG)
+    showTrace("Public and Private keys", keys, ENABLED_TRACE)
 
     enc = encrypt(message, keys, G, Zq, p)
-    print("Encrypted message", enc)
+    sys.stdout.write('\n\n')
+    print("Encrypted message: ", enc)
 
     dec = decrypt(enc,p,G,keys)
-
-    print("Decrypted message: " + dec)
+    sys.stdout.write('\n\n')
+    sys.stdout.buffer.write("Decrypted message: ".encode())
+    sys.stdout.buffer.write(dec.encode(DECODE))
+    sys.stdout.write('\n\n')
 
 def decrypt(enc,mod,G,keys):
     message = ''
@@ -70,61 +74,61 @@ def decrypt(enc,mod,G,keys):
 def encrypt(message, keys, G, Zq, p):
     encrypt = []
     for i in range(0, len(message)):
-        logTrace("message", message[i], LOG)
+        showTrace("message", message[i], ENABLED_TRACE)
         m = G[(message[i])]
-        logTrace("m", m, LOG)
+        showTrace("m", m, ENABLED_TRACE)
         cipher = cipherText(Zq, p, keys, m)
-        logTrace("CipherText", cipher, LOG)
+        showTrace("CipherText", cipher, ENABLED_TRACE)
         encrypt.append(cipher)
 
     return encrypt
 
 def cipherText(Zq, p, keys, m):
     r = random.choice(Zq)
-    logTrace("Random r", r, LOG)
+    showTrace("Random r", r, ENABLED_TRACE)
 
     g1 = keys[0][0]
-    logTrace("g1", g1, LOG)
+    showTrace("g1", g1, ENABLED_TRACE)
 
     u1 = powMod(g1,r,p)
-    logTrace('u1',u1, LOG)
+    showTrace('u1',u1, ENABLED_TRACE)
 
     g2 = keys[0][1]
-    logTrace('g2',g2, LOG)
+    showTrace('g2',g2, ENABLED_TRACE)
 
     u2 = powMod(g2,r,p)
-    logTrace('u2',u2, LOG)
+    showTrace('u2',u2, ENABLED_TRACE)
 
     h = keys[0][4]
-    logTrace('h',h, LOG)
+    showTrace('h',h, ENABLED_TRACE)
 
     e = (powMod(h,r,p)*m)%p
-    logTrace('e', e, LOG)
+    showTrace('e', e, ENABLED_TRACE)
 
     c = keys[0][2]
-    logTrace('c',c, LOG)
+    showTrace('c',c, ENABLED_TRACE)
 
     d1 = keys[0][3][0]
     d2 = keys[0][3][1]
     d3 = keys[0][3][2]
-    logTrace('d1',d1, LOG)
-    logTrace('d2',d2, LOG)
-    logTrace('d3',d3, LOG)
+    showTrace('d1',d1, ENABLED_TRACE)
+    showTrace('d2',d2, ENABLED_TRACE)
+    showTrace('d3',d3, ENABLED_TRACE)
 
     y1a = keys[1][2]
-    logTrace('y1',y1a, LOG)
+    showTrace('y1',y1a, ENABLED_TRACE)
     y2a = keys[1][3]
-    logTrace('y2',y2a, LOG)
+    showTrace('y2',y2a, ENABLED_TRACE)
 
     y1b = keys[1][4]
-    logTrace('y1b',y1b, LOG)
+    showTrace('y1b',y1b, ENABLED_TRACE)
     y2b = keys[1][5]
-    logTrace('y2b',y2b, LOG)
+    showTrace('y2b',y2b, ENABLED_TRACE)
 
     y1c = keys[1][6]
-    logTrace('y1c',y1c, LOG)
+    showTrace('y1c',y1c, ENABLED_TRACE)
     y2c = keys[1][7]
-    logTrace('y2c',y2c, LOG)
+    showTrace('y2c',y2c, ENABLED_TRACE)
 
     v = vGenerator(u1,u2,e,p, r, c, d1, d2, d3)
 
@@ -134,8 +138,8 @@ def keysGenerator(G,Zq,mod):
     g1 = random.choice(G)
     g2 = random.choice(G)
 
-    logTrace('G random choice g1',g1, LOG)
-    logTrace('G random choice g2',g2, LOG)
+    showTrace('G random choice g1',g1, ENABLED_TRACE)
+    showTrace('G random choice g2',g2, ENABLED_TRACE)
 
     x1 = random.choice(Zq)
     x2 = random.choice(Zq)
@@ -148,19 +152,19 @@ def keysGenerator(G,Zq,mod):
     y2c = random.choice(Zq)
     z = random.choice(Zq)
 
-    logTrace('Zq random choice x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z ',[x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z], LOG)
+    showTrace('Zq random choice x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z ',[x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z], ENABLED_TRACE)
 
     c = ((powMod(g1,x1,mod) * powMod(g2,x2,mod))%mod)
-    logTrace('c',c, LOG)
+    showTrace('c',c, ENABLED_TRACE)
 
     d1 = ((powMod(g1,y1a,mod) * powMod(g2,y2a,mod))%mod)
     d2 = ((powMod(g1,y1b,mod) * powMod(g2,y2b,mod))%mod)
     d3 = ((powMod(g1,y1c,mod) * powMod(g2,y2c,mod))%mod)
     d = [d1,d2,d3]
 
-    logTrace('d',d, LOG)
+    showTrace('d',d, ENABLED_TRACE)
     h = powMod(g1,z,mod)
-    logTrace('h',h, LOG)
+    showTrace('h',h, ENABLED_TRACE)
 
     publicKey = [g1,g2,c,d,h]
     privateKey = [x1,x2,y1a,y2a,y1b,y2b,y1c,y2c,z]
@@ -170,11 +174,11 @@ def keysGenerator(G,Zq,mod):
 def vGenerator(u1, u2, e, mod, r, c, d1, d2, d3):
     v = powMod(c, r, mod)
     v = ((v * powMod(d1,(u1*r), mod))%mod)
-    logTrace('v-u1',v, LOG)
+    showTrace('v-u1',v, ENABLED_TRACE)
     v = ((v * powMod(d2,(u2*r), mod))%mod)
-    logTrace('v-u2',v, LOG)
+    showTrace('v-u2',v, ENABLED_TRACE)
     v = ((v * powMod(d3,(e*r), mod))%mod)
-    logTrace('v-e',v, LOG)
+    showTrace('v-e',v, ENABLED_TRACE)
 
     return v
 
@@ -215,7 +219,7 @@ def generator(pow, mod):
     for i in range(2, mod):
         g = powMod(i,pow,mod)
         if g == 1:
-            logTrace('generator',i, LOG)
+            showTrace('generator',i, ENABLED_TRACE)
             return i
 
 def isPrime(n) :
@@ -230,8 +234,8 @@ def isPrime(n) :
 
     return True
 
-def logTrace(traceKey, traceValue, enable) :
+def showTrace(traceKey, traceValue, enable) :
     if (int(enable) == 1):
-        print(traceKey+" =>=>=> ", traceValue)
+        print(traceKey+" : ", traceValue)
 
 main()
